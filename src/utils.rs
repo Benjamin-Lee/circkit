ee1a8ab66034f5efedbf4bd0a88c0622c566b812
Benjamin Lee <benjamindlee@me.com>
2022-08-07 19:08:01 -0400

    Simplify reader and writers using Needletail

--- src/utils.rs
+++ src/utils.rs
@@ -1,67 +1,34 @@
 use anyhow::{bail, Result};
-use bio::io::fasta;
+use needletail::{parse_fastx_file, parse_fastx_stdin, FastxReader};
 use std::{
     fs::File,
-    io::{self, stdout, BufRead, BufWriter, Read, Write},
+    io::{stdout, BufWriter, Write},
     path::PathBuf,
 };
 
-pub fn get_bufwriter(output: &Option<PathBuf>) -> BufWriter<Box<dyn Write>> {
-    const BUFFER_CAPACITY: usize = 64 * 1024;
-
-    // decide where to write to
-    let stdout = stdout();
-    let file;
-    match output {
-        Some(path) => {
-            file = File::create(path).expect("Unable to create output file");
-            BufWriter::with_capacity(BUFFER_CAPACITY, Box::new(file))
-        }
-        None => BufWriter::with_capacity(BUFFER_CAPACITY, Box::new(stdout.lock())),
-    }
-}
-
-pub fn get_reader(input: &Option<PathBuf>) -> Box<dyn BufRead> {
-    let stdin = io::stdin();
+pub fn to_reader(input: &Option<PathBuf>) -> Result<Box<dyn FastxReader>> {
     match input {
-        Some(path) => Box::new(io::BufReader::new(
-            File::open(path).expect("Unable to create output file"),
-        )),
-        None => Box::new(stdin.lock()),
+        Some(filename) => Ok(parse_fastx_file(&filename).expect("valid path/file")),
+        None => {
+            if atty::is(atty::Stream::Stdin) {
+                bail!("No stdin detected. Did you mean to include a file argument?");
+            }
+            Ok(parse_fastx_stdin().expect("valid stdin"))
+        }
     }
 }
-
-// #[derive(Debug)]
-// pub enum Input {
-//     Stdin,
-//     File(PathBuf),
-// }
-
-// impl Input {
-//     ///hi
-//     pub fn from(path: Option<PathBuf>) -> Result<Self> {
-//         let input = match path {
-//             Some(path) => Input::File(path),
-//             None => Input::Stdin,
-//         };
-
-//         match input {
-//             Input::Stdin => {
-//                 if atty::is(atty::Stream::Stdin) {
-//                     bail!("No stdin detected. Did you mean to include a file argument?");
-//                 }
-//             }
-//             Input::File(ref path) => {
-//                 let file = File::open(path)?;
-//                 if !file.metadata()?.is_file() {
-//                     bail!(
-//                         "Input {} is a directory. Did you mean to include a file in the directory?",
-//                         path.display()
-//                     );
-//                 }
-//             }
-//         }
-
-//         Ok(input)
-//     }
-// }
+pub fn to_writer(output: &Option<PathBuf>) -> Result<Box<dyn Write>> {
+    match output {
+        Some(filename) => {
+            let file = File::create(filename)?;
+            if !file.metadata()?.is_file() {
+                bail!(
+                    "Input {} is a directory. Did you mean to include a file in the directory?",
+                    filename.display()
+                );
+            }
+            Ok(Box::new(BufWriter::new(file)))
+        }
+        None => Ok(Box::new(BufWriter::new(stdout()))),
+    }
+}
