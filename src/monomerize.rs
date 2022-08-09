bb07d2033a65b6293a64ba23ba3f4039f83e29c0
Benjamin Lee <benjamindlee@me.com>
2022-08-08 23:27:45 -0400

    Make the monomerization command actually work

--- src/monomerize.rs
+++ src/monomerize.rs
@@ -1,3 +1,4 @@
+use anyhow::bail;
 use needletail::parser::write_fasta;
 
 use crate::{
@@ -11,16 +12,59 @@ pub fn monomerize(cmd: &Command) -> anyhow::Result<()> {
             input,
             output,
             normalize,
+            seed_length,
+            max_mismatch,
+            min_identity,
+            require_overlap,
+            ..
         } => {
+            // region: some basic sanity checks
+            if max_mismatch.is_some() && min_identity.is_some() {
+                bail!("cannot specify both max_mismatch and min_identity");
+            }
+
+            // make sure the minimum identity is in range
+            if let Some(min_identity) = *min_identity {
+                if min_identity < 0.0 || min_identity > 1.0 {
+                    bail!("min_identity must be between 0.0 and 1.0");
+                }
+            }
+            // endregion
+
             let mut reader = to_reader(&input)?;
             let mut writer = to_writer(&output)?;
 
             while let Some(r) = reader.next() {
                 let record = r?;
 
+                let seq = &record.seq();
+
+                let monomer = if *normalize {
+                    circkit::normalize(
+                        circkit::monomerize(
+                            seq,
+                            usize::try_from(*seed_length)?,
+                            max_mismatch.unwrap_or(0),
+                        ),
+                        circkit::normalize::Alphabet::Dna,
+                    )
+                } else {
+                    circkit::monomerize(
+                        seq,
+                        usize::try_from(*seed_length)?,
+                        max_mismatch.unwrap_or(0),
+                    )
+                    .to_owned()
+                };
+
+                // if requested, only keep the monomer if it is greater than unit length
+                if *require_overlap && monomer.len() == seq.len() {
+                    continue;
+                }
+
                 write_fasta(
                     record.id(),
-                    circkit::monomerize(&record.seq(), 10, 1),
+                    &monomer,
                     &mut writer,
                     needletail::parser::LineEnding::Unix,
                 )?;
