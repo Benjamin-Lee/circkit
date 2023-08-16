ad43765c8cdfcaeaab2b54aac81005aff1506ffb
Benjamin Lee <benjamindlee@me.com>
2023-08-16 01:04:01 -0500

    move the monomerizer building out of the worker function

--- src/monomerize.rs
+++ src/monomerize.rs
@@ -48,28 +48,28 @@ pub fn monomerize(cmd: &Command) -> anyhow::Result<()> {
             let mut writer = output_to_writer(output)?;
             let mut table_writer = table_path_to_writer(table);
 
+            let mut builder = circkit::monomerize::Monomerizer::builder();
+
+            // set the seed length
+            builder.seed_len((*seed_length).try_into().expect("Seed length is too large"));
+
+            // set the maximum mismatch count
+            if let Some(max_mismatch) = *max_mismatch {
+                builder.overlap_dist(max_mismatch);
+            }
+
+            // set the minimum identity
+            if let Some(min_identity) = *min_identity {
+                builder.overlap_min_identity(min_identity);
+            }
+
+            let monomerizer = builder.build().unwrap();
+
             parallel_fasta(
                 reader,
                 *threads,
                 *batch_size,
                 |record, idx| {
-                    let mut builder = circkit::monomerize::Monomerizer::builder();
-
-                    // set the seed length
-                    builder.seed_len((*seed_length).try_into().expect("Seed length is too large"));
-
-                    // set the maximum mismatch count
-                    if let Some(max_mismatch) = *max_mismatch {
-                        builder.overlap_dist(max_mismatch);
-                    }
-
-                    // set the minimum identity
-                    if let Some(min_identity) = *min_identity {
-                        builder.overlap_min_identity(min_identity);
-                    }
-
-                    let m = builder.build().unwrap();
-
                     // normalize the sequence
                     let normalized = match needletail::sequence::normalize(record.seq(), false) {
                         Some(x) => x,
@@ -77,14 +77,14 @@ pub fn monomerize(cmd: &Command) -> anyhow::Result<()> {
                     };
 
                     // make sure the sequence is at least as long as the seed length
-                    if normalized.len() < m.seed_len {
+                    if normalized.len() < monomerizer.seed_len {
                         *idx = None;
                         return;
                     }
 
                     *idx = match sensitive {
-                        true => m.last_monomer_end_index_sensitive(&normalized),
-                        false => m.last_monomer_end_index(&normalized),
+                        true => monomerizer.last_monomer_end_index_sensitive(&normalized),
+                        false => monomerizer.last_monomer_end_index(&normalized),
                     }
                 },
                 |record, idx| {
